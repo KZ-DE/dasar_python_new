073c4ecfda50f2399173a608aed650ae10717fab
KZ-DE <nebulacom005@gmail.com>
2023-07-22 23:38:27 +0700

    yes

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-import controlpy
 
-# Contoh data respon sistem (waktu dan output)
+# Data respon sistem (waktu dan output)
 time = np.array([0, 1, 2, 3, 4, 5])
 output = np.array([0, 1, 3, 4, 3, 1])
 
 # Fungsi untuk menghitung kesalahan kuadrat antara data respon dan respon simulasi PID
 def objective(params):
     Kp, Ki, Kd = params
-    system = controlpy.TransferFunction([Kp], [1, 0]) + controlpy.TransferFunction([Ki], [1]) / s + controlpy.TransferFunction([Kd * s], [1])
-    _, y = controlpy.forced_response(system, time, output)
-    return np.sum((y - output) ** 2)
+    y = np.zeros_like(output)
+    error_sum = 0
+
+    # Simulasi respon sistem dengan parameter PID yang diberikan
+    for i in range(1, len(output)):
+        dt = time[i] - time[i-1]
+        error = output[i] - y[i-1]
+        y[i] = y[i-1] + (Kp * error) + (Ki * error * dt) + (Kd * (error - (output[i-1] - y[i-1])) / dt)
+        error_sum += error ** 2
+
+    return error_sum
 
 # Inisialisasi parameter awal untuk optimisasi
 initial_guess = [1, 1, 1]
@@ -25,3 +32,11 @@ Kp_opt, Ki_opt, Kd_opt = result.x
 
 # Cetak hasil tuning PID
 print(f"Kp: {Kp_opt}, Ki: {Ki_opt}, Kd: {Kd_opt}")
+
+# Plot respon sistem asli dan hasil simulasi PID setelah tuning
+plt.plot(time, output, label='Data Respon Sistem Asli')
+plt.plot(time, y, label='Respon Simulasi PID')
+plt.xlabel('Waktu')
+plt.ylabel('Output')
+plt.legend()
+plt.show()
